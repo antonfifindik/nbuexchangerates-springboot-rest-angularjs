ec45138e4317d1a6d606e4a44f97c476503bcf12
Anton Fifindik <fifindik.av@gmail.com>
2018-04-11 17:39:06 +0300

    changed api address in main.js

--- src/main/resources/public/js/main.js
+++ src/main/resources/public/js/main.js
@@ -1,7 +1,7 @@
 var app = angular.module("springDemo", []);
 app.controller("AppCtrl", function ($scope, $http) {
     $scope.exchanges = [];
-    $http.get('http://localhost:8080/api/exchange').then(function (response) {
+    $http.get('/api/exchange').then(function (response) {
         $scope.exchanges = response.data;
     })
 });
